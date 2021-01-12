49029795d31011fb86a8b0e0e4018375e231faec
Oleksandr Perepelytsya <o.perepelytsya@gmail.com>
2021-01-12 14:51:45 +0000

    Update DealsAndPromotionsPage.java

--- ATQA-2020/src/main/java/com/atqa2020Kris/Pages/DealsAndPromotionsPage.java
+++ ATQA-2020/src/main/java/com/atqa2020Kris/Pages/DealsAndPromotionsPage.java
@@ -6,12 +6,15 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class DealsAndPromotionsPage extends Page{
 
     private final String todayDealsLocator = "//div[@id='nav-xshop']//a[@href='/international-sales-offers/b/?ie=UTF8&node=15529609011&ref_=nav_cs_gb_intl']";
+    //TODO no need to declare private variable as static here
     private static String DEALS_PAGE_TITLE = "Deals and Promotions";
     private DealsAndPromotionsPage dealsAndPromotionsPage;
 
     public DealsAndPromotionsPage(RemoteWebDriver driver) {
         super(driver);
     }
+    
+    //TODO can we please extract locator into a variable?
     public boolean isItDealsAndPromotionsPage(String dealsAndPromotions) {
         return driver.findElement(By.xpath("//div[@class='fst-h1-st pageBanner']//b[contains(text(),'Deals and Promotions')]")).getText().equals(DEALS_PAGE_TITLE);
     }
